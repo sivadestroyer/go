4bb7ff635722f672ef76dea05532374e117d2dd3
SIVABALAMURUGAN M <sbmsakthi007@gmail.com>
2023-05-10 10:39:46 +0530

    variables in go

---
+++ var.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a int = 3
+	var b string = "murugan Aswin"
+	fmt.Println(a)
+	fmt.Println(b)
+}
---
+++ var2.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	a, b := 2, 3
+	var c, d int = 7, 8
+	fmt.Println(a, b, c, d)
+
+}
